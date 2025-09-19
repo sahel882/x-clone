945e5a2fc5d99e95b3a920da1b2af48784974eb3
maneshadesilva99-code <maneshadesilva99@gmail.com>
2025-09-19 17:20:32 +0530

    startserver implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,24 @@
 import express from 'express';
-import {ENV} from "./config/env.js";
-import {connectDB} from "./config/db.js";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 import "dotenv/config";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res)=>{
-   res.send("Hello from API");
+app.get("/", (req, res) => {
+    res.send("Hello from API");
 });
 
-app.listen(ENV.PORT, () => {
-    connectDB();
-    console.log('Server is running on port: ', ENV.PORT);
-})
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.listen(ENV.PORT, () => console.log('Server is running on port: ', ENV.PORT));
+    } catch (error) {
+        console.log("Failed to start server: ", error.message);
+        process.exit(1);
+    }
+};
+
+startServer();
