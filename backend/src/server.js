096e2b24f34fa6546b5e06c68abcdc97627c227b
maneshadesilva99-code <maneshadesilva99@gmail.com>
2025-09-19 21:17:54 +0530

    errors handeld

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 app.use("/api/users", userRoutes);
 app.use("/api/post", postRoutes);
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
     console.error("Unhandled error:", err);
     res.status(500).json({ error: err.message || "Internal server error" });
 });
