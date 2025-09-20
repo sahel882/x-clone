8f1a48ebe2c9616f0d50e5cb07bbc6e4102c55ab
maneshadesilva99-code <maneshadesilva99@gmail.com>
2025-09-20 18:16:23 +0530

    tabs done

---
+++ mobile/components/SignOutButton.tsx
@@ -0,0 +1,17 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { Feather } from '@expo/vector-icons'
+import { useSignOut } from '@/hooks/useSignOut'
+
+const SignOutButton = () => {
+
+  const { handleSignOut } = useSignOut()
+
+  return (
+    <TouchableOpacity onPress={handleSignOut}>
+      <Feather name='log-out' size={24} color={"#E0245E"} />
+    </TouchableOpacity>
+  )
+}
+
+export default SignOutButton
